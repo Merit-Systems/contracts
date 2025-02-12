714f1a9c7848f0a93c0b55ceb48d7cccebd90365
shafu <shafu0x@gmail.com>
2025-02-12 19:24:18 +0000

    test base

---
+++ test/Base.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+import "forge-std/Test.sol";
+
+import {MeritLedger} from "src/MeritLedger.sol";
+import {Deploy}      from "../script/Deploy.s.sol";
+
+contract Base_Test is Test {
+
+    MeritLedger ledger;
+
+    function setUp() public {
+        Deploy deploy = new Deploy();
+        ledger = MeritLedger(deploy.run());
+    }
+}
