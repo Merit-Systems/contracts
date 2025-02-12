8905b92be08939d0c67caf2ee3223f40ded22ba7
shafu <shafu0x@gmail.com>
2025-02-12 21:44:36 +0000

    more errors

--- libraries/Errors.sol
+++ libraries/Errors.sol
@@ -2,9 +2,14 @@
 pragma solidity =0.8.26;
 
 library Errors {
-    string internal constant NOT_OWNER           = "Not Owner";
-    string internal constant NOT_SUPPORTED       = "Not Supported";
-    string internal constant ALREADY_INITIALIZED = "Already Initialized";
-    string internal constant LENGTH_MISMATCH     = "Length Mismatch";
-    string internal constant ZERO_SHARE          = "Zero Share";
+    string internal constant NOT_OWNER            = "Not Owner";
+    string internal constant NOT_SUPPORTED        = "Not Supported";
+    string internal constant ALREADY_INITIALIZED  = "Already Initialized";
+    string internal constant LENGTH_MISMATCH      = "Length Mismatch";
+    string internal constant ZERO_SHARE           = "Zero Share";
+    string internal constant NO_TIME_ELAPSED      = "No Time Elapsed";
+    string internal constant NO_NEW_MINTED_SHARES = "No New Minted Shares";
+    string internal constant NO_PULL_REQUESTS     = "No Pull Requests";
+    string internal constant NO_WEIGHTS           = "No Weights";
+
 }
