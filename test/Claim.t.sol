fcff5dbfd4ad02efe3ebe45c46ed633f058424a5
shafu <shafu0x@gmail.com>
2025-02-13 10:05:36 +0000

    check for claim errors

--- test/Claim.t.sol
+++ test/Claim.t.sol
@@ -15,11 +15,59 @@ contract Claim_Test is Base_Test {
     {
         uint repoId = 0;
         usdc.mint(address(ledger), 100e18);
+        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
+
         vm.startPrank(alice);
         vm.warp(block.timestamp + 365 days);
 
-        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
         ledger.addMerkleRoot(repoId, merkleRoot);
+        ledger.claim(repoId, 0, alice, 100e18, proof, merkleRoot);
+    }
+
+    function test_claim_fail_notAccountOwner() 
+        public 
+        _init
+    {
+        uint repoId = 0;
+        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
+
+        vm.startPrank(alice);
+
+        ledger.addMerkleRoot(repoId, merkleRoot);
+
+        expectRevert(Errors.NOT_ACCOUNT_OWNER);
+        ledger.claim(repoId, 0, bob, 100e18, proof, merkleRoot);
+    }
+
+    function test_claim_fail_invalidRoot() 
+        public 
+        _init
+    {
+        uint repoId = 0;
+        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
+
+        vm.startPrank(alice);
+
+        // ledger.addMerkleRoot(repoId, merkleRoot);
+
+        expectRevert(Errors.INVALID_ROOT);
+        ledger.claim(repoId, 0, alice, 100e18, proof, merkleRoot);
+    }
+
+    function test_claim_fail_alreadyClaimed() 
+        public 
+        _init
+    {
+        uint repoId = 0;
+        usdc.mint(address(ledger), 100e18);
+        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
+
+        vm.startPrank(alice);
+
+        ledger.addMerkleRoot(repoId, merkleRoot);
+        ledger.claim(repoId, 0, alice, 100e18, proof, merkleRoot);
+
+        expectRevert(Errors.ALREADY_CLAIMED);
         ledger.claim(repoId, 0, alice, 100e18, proof, merkleRoot);
     }
 }
