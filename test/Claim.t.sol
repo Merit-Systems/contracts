b51c426758f87b6baaa2427da6318198e4701c1d
shafu <shafu0x@gmail.com>
2025-02-13 07:32:21 +0000

    claim test Ä

--- test/Claim.t.sol
+++ test/Claim.t.sol
@@ -13,8 +13,13 @@ contract Claim_Test is Base_Test {
         public 
         _init
     {
-        // deal(Params.MAINNET_USDC, address(ledger), 100e18);
-        // vm.startPrank(alice);
-        // vm.warp(block.timestamp + 365 days);
+        uint repoId = 0;
+        usdc.mint(address(ledger), 100e18);
+        vm.startPrank(alice);
+        vm.warp(block.timestamp + 365 days);
+
+        (bytes32 merkleRoot, bytes32[] memory proof) = getSingleLeafRootAndProof(0, alice, 100e18);
+        ledger.setMerkleRoot(repoId, merkleRoot);
+        ledger.claim(repoId, 0, alice, 100e18, proof);
     }
 }
