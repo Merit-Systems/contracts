4593925a96e3c36eea6f2567853af31e0296c903
shafu <shafu0x@gmail.com>
2025-02-19 17:03:48 -0500

    refactor duration

--- src/Split.sol
+++ src/Split.sol
@@ -5,8 +5,8 @@ interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
-contract Disperse {
-    function disperse(IERC20 token, address[] calldata recipients, uint256[] calldata values) external {
+contract Split {
+    function split(IERC20 token, address[] calldata recipients, uint256[] calldata values) external {
         for (uint256 i = 0; i < recipients.length; i++) {
             require(token.transferFrom(msg.sender, recipients[i], values[i]));
         }
