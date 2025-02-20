a1b91a4ac356a5225dda7cd0e8268341b94bf1ae
shafu <shafu0x@gmail.com>
2025-02-20 11:26:43 -0500

    refactor

--- src/Payments/SplitWithLockup.sol
+++ src/Payments/SplitWithLockup.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.26;
 
 import {Owned} from "solmate/auth/Owned.sol";
-
-interface IERC20 {
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-}
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract SplitWithLockup is Owned(msg.sender) {
     mapping(address => bool) public canClaim;
@@ -15,49 +11,43 @@ contract SplitWithLockup is Owned(msg.sender) {
         canClaim[recipient] = allowed;
     }
 
-    uint256 public claimPeriod = 90 days; // 3 months
-
     struct Deposit {
         uint256 amount;
-        IERC20 token;
+        ERC20   token;
         address recipient;
         address sender;
         uint256 claimDeadline;
-        bool claimed;
+        bool    claimed;
     }
 
     uint256 public depositCount;
     mapping(uint256 => Deposit) public deposits;
 
     function split(
-        IERC20 token,
+        ERC20 token,
         address[] calldata recipients,
         uint256[] calldata values,
-        bool[] calldata canTransferNow
+        bool[]    calldata canTransferNow,
+        uint256[] calldata claimPeriods
     ) external {
         require(
             recipients.length == values.length && 
-            recipients.length == canTransferNow.length
+            recipients.length == canTransferNow.length &&
+            recipients.length == claimPeriods.length
         );
 
         for (uint256 i = 0; i < recipients.length; i++) {
             if (canTransferNow[i]) {
-                // Transfer directly to the recipient
-                require(
-                    token.transferFrom(msg.sender, recipients[i], values[i])
-                );
+                require(token.transferFrom(msg.sender, recipients[i], values[i]));
             } else {
-                // Transfer into the contract and record a deposit
-                require(
-                    token.transferFrom(msg.sender, address(this), values[i])
-                );
+                require(token.transferFrom(msg.sender, address(this), values[i]));
 
                 deposits[depositCount] = Deposit({
                     amount: values[i],
                     token: token,
                     recipient: recipients[i],
                     sender: msg.sender,
-                    claimDeadline: block.timestamp + claimPeriod,
+                    claimDeadline: block.timestamp + claimPeriods[i],
                     claimed: false
                 });
 
@@ -73,14 +63,10 @@ contract SplitWithLockup is Owned(msg.sender) {
         require(deposit.amount > 0);
         require(msg.sender == deposit.recipient);
         require(block.timestamp <= deposit.claimDeadline);
-
-        // Check if the owner has granted permission to this recipient
         require(canClaim[msg.sender]);
 
         deposit.claimed = true;
-        require(
-            deposit.token.transfer(msg.sender, deposit.amount)
-        );
+        require(deposit.token.transfer(msg.sender, deposit.amount));
     }
 
     function reclaim(uint256 depositId) external {
