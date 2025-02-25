b801d69c50e7fc26fc79cdd86c839339af80e3f0
shafu <shafu0x@gmail.com>
2025-02-25 07:40:02 -0700

    simplify

--- script/Deploy.SplitWithLockup.sol
+++ script/Deploy.SplitWithLockup.sol
@@ -3,12 +3,11 @@ pragma solidity ^0.8.26;
 
 import {SplitWithLockup} from "../src/Payments/SplitWithLockup.sol";
 import {Script} from "forge-std/Script.sol";
-import {Params} from "../libraries/Params.sol";
 
 contract DeploySplitWithLockup is Script {
     function run() public {
         vm.startBroadcast();
-        new SplitWithLockup(Params.SEPOLIA_ENTRYPOINT);
+        new SplitWithLockup();
         vm.stopBroadcast();
     }
 }
--- src/Payments/SplitWithLockup.sol
+++ src/Payments/SplitWithLockup.sol
@@ -1,19 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Owned} from "solmate/auth/Owned.sol";
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC20}           from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
-import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
-contract SplitWithLockup is Context, Owned(msg.sender) {
+contract SplitWithLockup {
     using SafeTransferLib for ERC20;
-
-    mapping(address => bool) public canClaim;
-
-    function setCanClaim(address recipient, bool allowed) external onlyOwner {
-        canClaim[recipient] = allowed;
-    }
 
     struct Deposit {
         uint    amount;
@@ -32,12 +24,7 @@ contract SplitWithLockup is Context, Owned(msg.sender) {
         uint    value;
         bool    canTransferNow;
         uint    claimPeriod;
-    }
-
-    address public immutable entryPoint;
-
-    constructor(address _entryPoint) {
-        entryPoint = _entryPoint;
+        address sender;
     }
 
     function split(
@@ -50,13 +37,13 @@ contract SplitWithLockup is Context, Owned(msg.sender) {
             } else {
                 token.safeTransferFrom(msg.sender, address(this), params[i].value);
 
-                deposits[depositCount] = Deposit({
-                    amount: params[i].value,
-                    token: token,
-                    recipient: params[i].recipient,
-                    sender: msg.sender,
+                deposits[depositCount++] = Deposit({
+                    amount:        params[i].value,
+                    token:         token,
+                    recipient:     params[i].recipient,
+                    sender:        params[i].sender,
                     claimDeadline: block.timestamp + params[i].claimPeriod,
-                    claimed: false
+                    claimed:       false
                 });
 
                 depositCount++;
@@ -68,10 +55,7 @@ contract SplitWithLockup is Context, Owned(msg.sender) {
         Deposit storage deposit = deposits[depositId];
 
         require(!deposit.claimed);
-        // require(deposit.amount > 0);
-        // require(_msgSender() == deposit.recipient);
         require(block.timestamp <= deposit.claimDeadline);
-        // require(canClaim[msg.sender]);
 
         deposit.claimed = true;
         deposit.token.safeTransfer(deposit.recipient, deposit.amount);
@@ -81,41 +65,9 @@ contract SplitWithLockup is Context, Owned(msg.sender) {
         Deposit storage deposit = deposits[depositId];
 
         require(!deposit.claimed);
-        require(deposit.amount > 0);
-        require(msg.sender == deposit.sender);
         require(block.timestamp > deposit.claimDeadline);
 
         deposit.claimed = true;
-        deposit.token.safeTransfer(msg.sender, deposit.amount);
-    }
-
-    /// 🔹 Override `_msgSender()` to support ERC-4337
-    function _msgSender() internal view override returns (address sender) {
-        // if (msg.sender == 0x99ecA80b4Ebf8fDACe6627BEcb75EF1e620E6956 
-        // || msg.sender == 0x4337006f33e2940FcbEbD899bF2396117E65dF9B 
-        // || msg.sender == 0x0000000000000039cd5e8aE05257CE51C473ddd1 
-        // || msg.sender == 0x845ADb2C711129d4f3966735eD98a9F09fC4cE57 
-        // || msg.sender == 0xBAC849bB641841b44E965fB01A4Bf5F074f84b4D 
-        // || msg.sender == 0xf384FddCAf70336dcA46404D809153A0029A0253 
-        // || msg.sender == 0x0000000071727De22E5E9d8BAf0edAc6f37da032 
-        // || msg.sender == 0x74cb5e4ee81b86e70f9045036a1c5477de69ee87 
-        // || msg.sender == 0x4337000c2828F5260d8921fD25829F606b9E8680) {
-        if (true) {
-            // Extract the actual user address from `msg.data`
-            assembly {
-                sender := shr(96, calldataload(sub(calldatasize(), 20)))
-            }
-        } else {
-            sender = msg.sender;
-        }
-    }
-    
-    function _msgData() internal view override returns (bytes calldata) {
-        if (msg.sender == entryPoint) {
-            // strip off the final 20 bytes that contain the real sender
-            return msg.data[:msg.data.length - 20];
-        } else {
-            return msg.data;
-        }
+        deposit.token.safeTransfer(deposit.sender, deposit.amount);
     }
 }
