a3f587598941a3f8ac8c2220e82a52c45f59dc3d
shafu <shafu0x@gmail.com>
2025-03-04 14:27:01 -0500

    can-claim

--- src/Payments/SplitWithLockup.sol
+++ src/Payments/SplitWithLockup.sol
@@ -7,6 +7,10 @@ import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 contract SplitWithLockup {
     using SafeTransferLib for ERC20;
 
+    address public immutable owner;
+
+    mapping(address => bool) public canClaim;
+
     struct Deposit {
         uint    amount;
         ERC20   token;
@@ -18,7 +22,7 @@ contract SplitWithLockup {
 
     uint public depositCount;
     mapping(uint => Deposit) public deposits;
-    
+
     mapping(address => uint[]) public senderDeposits;
     mapping(address => uint[]) public recipientDeposits;
 
@@ -29,6 +33,8 @@ contract SplitWithLockup {
         uint    claimPeriod;
         address sender;
     }
+
+    constructor() { owner = msg.sender; }
 
     function split(
         ERC20 token,
@@ -49,7 +55,6 @@ contract SplitWithLockup {
                     claimed:       false
                 });
 
-                // Track deposits for both sender and recipient
                 senderDeposits[params[i].sender].push(depositCount);
                 recipientDeposits[params[i].recipient].push(depositCount);
 
@@ -61,8 +66,10 @@ contract SplitWithLockup {
     function claim(uint depositId) external {
         Deposit storage deposit = deposits[depositId];
 
-        require(!deposit.claimed);
-        require(block.timestamp <= deposit.claimDeadline);
+        // require(!deposit.claimed);
+        // require(block.timestamp <= deposit.claimDeadline);
+        require(deposit.recipient == msg.sender);
+        require(canClaim[msg.sender]);
 
         deposit.claimed = true;
         deposit.token.safeTransfer(deposit.recipient, deposit.amount);
@@ -73,9 +80,31 @@ contract SplitWithLockup {
 
         require(!deposit.claimed);
         require(block.timestamp > deposit.claimDeadline);
+        // require(deposit.sender == msg.sender);
 
         deposit.claimed = true;
         deposit.token.safeTransfer(deposit.sender, deposit.amount);
+    }
+
+    function setCanClaim(
+        address recipient,
+        bool status,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked("setCanClaim", address(this), recipient, status)
+        );
+
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+        );
+
+        address signer = ecrecover(ethSignedMessageHash, v, r, s);
+        require(signer == owner, "Invalid signature: Not the owner");
+
+        canClaim[recipient] = status;
     }
 
     function getDepositsBySender(address sender) external view returns (uint[] memory) {
