075f6b60d8f22a3259c1971e1ec0038e2318400c
shafu <shafu0x@gmail.com>
2025-03-17 09:49:32 -0400

    version into bytes

--- src/Payments/Escrow.sol
+++ src/Payments/Escrow.sol
@@ -224,7 +224,7 @@ contract Escrow is Owned, IEscrow {
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256(bytes("Escrow")), 
-                keccak256("1"),
+                keccak256(bytes("1")),
                 block.chainid,
                 address(this)
             )
