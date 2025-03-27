b12858179082a1624d4b014d81c8f2f769d5a801
shafu <shafu0x@gmail.com>
2025-03-27 14:30:33 -0400

    mock erc20

--- script/Deploy.Sepolia.sol
+++ script/Deploy.Sepolia.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Deploy} from "./Deploy.s.sol";
-import {Params} from "../libraries/Params.sol";
+import {Deploy}    from "./Deploy.s.sol";
+import {Params}    from "../libraries/Params.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 
 contract DeploySepolia {
     function run() public {
-        address[] memory initialWhitelistedTokens = new address[](2);
-        initialWhitelistedTokens[0] = Params.SEPOLIA_WETH;
-        initialWhitelistedTokens[1] = Params.SEPOLIA_USDC;
+      MockERC20 testToken = new MockERC20("USDC", "USDC", 6);
 
-        new Deploy().deploy(Params.OWNER, initialWhitelistedTokens);
+      address[] memory initialWhitelistedTokens = new address[](2);
+      initialWhitelistedTokens[0] = Params.SEPOLIA_WETH;
+      initialWhitelistedTokens[1] = Params.SEPOLIA_USDC;
+      initialWhitelistedTokens[2] = address(testToken);
+
+      new Deploy().deploy(Params.OWNER, initialWhitelistedTokens);
     }
 }
