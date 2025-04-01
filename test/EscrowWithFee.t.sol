f54c5404bb6de086918ef7f66cfe1cb2daa90f95
shafu <shafu0x@gmail.com>
2025-04-01 13:07:37 -0400

    escrow-with-fee

--- test/EscrowWithFee.t.sol
+++ test/EscrowWithFee.t.sol
@@ -26,7 +26,7 @@ contract Base_Test is Test {
     function setUp() public {
         address[] memory initialWhitelistedTokens = new address[](1);
         initialWhitelistedTokens[0] = address(wETH);
-        escrow = new Deploy().deploy(owner, initialWhitelistedTokens, 0);
+        escrow = new Deploy().deploy(owner, initialWhitelistedTokens, Params.BASE_FEE_BPS);
 
         alice = makeAddr("alice");
         bob   = makeAddr("bob");
@@ -39,38 +39,15 @@ contract Base_Test is Test {
     function test_deposit() public {
         uint amount = 1000000000000000000;
         uint depositId = deposit(amount, alice);
-        assertEq(wETH.balanceOf(address(escrow)), amount);
+        assertEq(wETH.balanceOf(address(escrow)), 997500000000000000);
         assertEq(depositId, 0);
-    }
-
-    function test_setCanClaim() public {
-        setCanClaim(alice, true, 1 days);
-        assertEq(escrow.canClaim(alice), true);
     }
 
     function test_claim() public {
         uint depositId = deposit(1000000000000000000, alice);
         (uint8 v, bytes32 r, bytes32 s) = generateSignature(alice, true);
         escrow.claim(depositId, alice, true, 1 days, v, r, s);
-        assertEq(wETH.balanceOf(alice), 1000000000000000000);
-    }
-
-    function test_claimTwice() public {
-        uint depositId = deposit(1000000000000000000, alice);
-        (uint8 v, bytes32 r, bytes32 s) = generateSignature(alice, true);
-        escrow.claim(depositId, alice, true, 1 days, v, r, s);
-
-        uint depositId2 = deposit(1000000000000000000, alice);
-        (v, r, s) = generateSignature(alice, true);
-        escrow.claim(depositId2, alice, true, 1 days, v, r, s);
-    }
-
-    function test_claim_failAlreadyClaimed() public {
-        uint depositId = deposit(1000000000000000000, alice);
-        (uint8 v, bytes32 r, bytes32 s) = generateSignature(alice, true);
-        escrow.claim(depositId, alice, true, 1 days, v, r, s);
-        expectRevert(Errors.ALREADY_CLAIMED);
-        escrow.claim(depositId, alice, true, 1 days, v, r, s);
+        assertEq(wETH.balanceOf(alice), 997500000000000000);
     }
 
     function test_reclaim() public {
@@ -78,21 +55,7 @@ contract Base_Test is Test {
         vm.warp(block.timestamp + 2 days);
         assertEq(wETH.balanceOf(bob), 0);
         escrow.reclaim(depositId);
-        assertEq(wETH.balanceOf(bob), 1000000000000000000);
-    }
-
-    function test_reclaim_failStillClaimable() public {
-        uint depositId = deposit(1000000000000000000, alice);
-        expectRevert(Errors.STILL_CLAIMABLE);
-        escrow.reclaim(depositId);
-    }
-
-    function test_reclaim_failAlreadyClaimed() public {
-        uint depositId = deposit(1000000000000000000, alice);
-        vm.warp(block.timestamp + 2 days);
-        escrow.reclaim(depositId);
-        expectRevert(Errors.ALREADY_CLAIMED);
-        escrow.reclaim(depositId);
+        assertEq(wETH.balanceOf(bob), 997500000000000000);
     }
 
     function test_batchReclaim() public {
@@ -101,7 +64,7 @@ contract Base_Test is Test {
         vm.warp(block.timestamp + 2 days);
         assertEq(wETH.balanceOf(bob), 0);
         escrow.batchReclaim(depositIds);
-        assertEq(wETH.balanceOf(bob), 1000000000000000000);
+        assertEq(wETH.balanceOf(bob), 997500000000000000);
     }
 
     function deposit(uint amount, address recipient) public returns (uint depositId) {
@@ -117,33 +80,6 @@ contract Base_Test is Test {
             claimPeriod: 1 days
         });
         return escrow.deposit(params[0]);
-    }
-
-    function setCanClaim(address recipient, bool status, uint256 deadline) public {
-        bytes32 structHash = keccak256(
-            abi.encode(
-                escrow.CLAIM_TYPEHASH(),
-                recipient, 
-                status,  
-                escrow.recipientNonces(recipient),
-                deadline
-            )
-        );
-
-        bytes32 digest = keccak256(
-            abi.encodePacked("\x19\x01", escrow.CLAIM_DOMAIN_SEPARATOR(), structHash)
-        );
-    
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
-
-        escrow.setCanClaim(
-            recipient,
-            status,
-            deadline,
-            v,
-            r,
-            s
-        );
     }
 
     function generateSignature(address recipient, bool status) public view returns (uint8 v, bytes32 r, bytes32 s) {
