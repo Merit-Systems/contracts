0a40f3a6017773d6a7ae894b9d1cf5225d364fe5
shafu <shafu0x@gmail.com>
2025-04-10 10:43:29 -0400

    gas script

---
+++ script/utils/Gas.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+
+import {Deploy}    from "../Deploy.s.sol";
+import {Params}    from "../../libraries/Params.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {Script}        from "forge-std/Script.sol";
+import {Escrow}        from "../../src/Payments/Escrow.sol";
+import {DepositParams} from "../../src/Payments/Escrow.sol";
+
+contract DeploySepolia is Script {
+    uint256[] NUM_DEPOSITS = [1, 10, 20, 50, 100, 500, 1000];
+
+    function run() public {
+        MockERC20 mockUSDC = new MockERC20("USD Coin", "USDC", 6);
+        mockUSDC.mint(Params.SEPOLIA_TESTER, 1000000000000000 * 10**6);
+
+        address[] memory initialWhitelistedTokens = new address[](3);
+        initialWhitelistedTokens[0] = Params.SEPOLIA_WETH;
+        initialWhitelistedTokens[1] = Params.SEPOLIA_USDC;
+        initialWhitelistedTokens[2] = address(mockUSDC);
+
+        Escrow escrow = new Deploy().deploy(Params.OWNER, initialWhitelistedTokens, 0);
+
+        for (uint256 j = 0; j < NUM_DEPOSITS.length; j++) {
+            uint256 numDeposits = NUM_DEPOSITS[j];
+            DepositParams[] memory depositParams = new DepositParams[](numDeposits);
+            uint256 baseAmount = 1000000000000000 * 10**6;
+            
+            // Create deposits with varying amounts
+            for (uint256 i = 0; i < numDeposits; i++) {
+                depositParams[i] = DepositParams({
+                    token: mockUSDC,
+                    sender: Params.SEPOLIA_TESTER,
+                    recipient: Params.SEPOLIA_TESTER,
+                    amount: baseAmount * (i + 1), // Each deposit will be a multiple of the base amount
+                    claimPeriod: 1000000000000000 * 10**6
+                });
+            }
+
+            // Mint and approve enough tokens for all deposits
+            uint256 totalAmount = baseAmount * ((numDeposits * (numDeposits + 1)) / 2); // Sum of 1+2+3+...+numDeposits
+            mockUSDC.mint(address(this), totalAmount);
+            mockUSDC.approve(address(escrow), totalAmount);
+            
+            uint256 gasBefore = gasleft();
+            escrow.batchDeposit(depositParams, 1, block.timestamp);
+            uint256 gasAfter = gasleft();
+            console.log("Gas used for %d deposits:", numDeposits, gasBefore - gasAfter);
+        }
+    }
+}
