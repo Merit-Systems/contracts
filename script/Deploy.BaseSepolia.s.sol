a03b715ccee9f6402410fd1cd8bd6ca40e733705
shafu <shafu0x@gmail.com>
2025-04-16 11:26:03 -0400

    refactor

--- script/Deploy.BaseSepolia.s.sol
+++ script/Deploy.BaseSepolia.s.sol
@@ -1,25 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Deploy}    from "./Deploy.s.sol";
-import {Params}    from "../libraries/Params.sol";
-import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
-import {Script} from "forge-std/Script.sol";
+import {Deploy}         from "./Deploy.s.sol";
+import {Params}         from "../libraries/Params.sol";
+import {MockERC20}      from "solmate/test/utils/mocks/MockERC20.sol";
+import {Script}         from "forge-std/Script.sol";
+import {CreatePayments} from "./utils/CreatePayments.s.sol";
+import {Escrow}         from "../src/Escrow.sol";
 
 contract DeployBaseSepolia is Script {
+  uint constant AMOUNT_TO_MINT = 100_000_000 * 10**6;
+
     function run() public {
       vm.startBroadcast();
 
       MockERC20 mockUSDC = new MockERC20("USD Coin", "USDC", 6);
-      mockUSDC.mint(Params.BASE_SEPOLIA_TESTER, 1000000000000000 * 10**6);
+      mockUSDC.mint(Params.BASESEPOLIA_TESTER,       AMOUNT_TO_MINT);
+      mockUSDC.mint(Params.BASESEPOLIA_TESTER_JSON,  AMOUNT_TO_MINT);
+      mockUSDC.mint(Params.BASESEPOLIA_TESTER_SHAFU, AMOUNT_TO_MINT);
 
       vm.stopBroadcast();
 
       address[] memory initialWhitelistedTokens = new address[](3);
-      initialWhitelistedTokens[0] = Params.BASE_SEPOLIA_WETH;
-      initialWhitelistedTokens[1] = Params.BASE_SEPOLIA_USDC;
+      initialWhitelistedTokens[0] = Params.BASESEPOLIA_WETH;
+      initialWhitelistedTokens[1] = Params.BASESEPOLIA_USDC;
       initialWhitelistedTokens[2] = address(mockUSDC);
 
-      new Deploy().deploy(Params.OWNER, initialWhitelistedTokens, 0);
+      Escrow escrow = new Deploy().deploy(Params.OWNER, initialWhitelistedTokens, 0);
+
+      new CreatePayments().deploy(
+        address(escrow),
+        address(mockUSDC),
+        5,
+        100 * 10**6,
+        Params.BASESEPOLIA_TESTER_SHAFU,
+        Params.BASESEPOLIA_TESTER_JSON
+      );
     }
 }
