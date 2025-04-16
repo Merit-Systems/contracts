6529a5414ac0f0083fab0ba9f49a909014282ed3
shafu <shafu0x@gmail.com>
2025-04-16 12:04:07 -0400

    test deploy

--- script/Deploy.Base.s.sol
+++ script/Deploy.Base.s.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.26;
 import {Deploy} from "./Deploy.s.sol";	
 import {Params} from "../libraries/Params.sol";	
 import {Script} from "forge-std/Script.sol";	
+import {Escrow} from "../src/Escrow.sol";
 
 contract DeployBase is Deploy {	
-    function run() public {	
+    function run() public returns (Escrow escrow) {	
         address[] memory initialWhitelistedTokens = new address[](1);	
         initialWhitelistedTokens[0] = Params.BASE_USDC;	
 
-        deploy(Params.OWNER, initialWhitelistedTokens, Params.BASE_FEE_BPS);	
+        escrow = deploy(Params.OWNER, initialWhitelistedTokens, Params.BASE_FEE_BPS);	
     }	
 }
---
+++ test/Deploy.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+import "forge-std/Test.sol";
+import {MockERC20}  from "solmate/test/utils/mocks/MockERC20.sol";
+import {DeployBase} from "../script/Deploy.Base.s.sol";
+import {Params}     from "../libraries/Params.sol";
+import {Escrow}     from "../src/Escrow.sol";
+
+contract Deploy_Test is Test {
+    DeployBase deployer;
+
+    function setUp() public {
+        deployer = new DeployBase();
+    }
+
+    function test_deploy() public {
+        Escrow escrow = deployer.run();
+
+        assertTrue(address(escrow) != address(0),                "Escrow not deployed");
+        assertTrue(escrow.isTokenWhitelisted(Params.BASE_USDC),  "WETH not whitelisted");
+        assertEq  (escrow.owner(), Params.OWNER,                 "Incorrect owner");
+        assertEq  (escrow.feeRecipient(), Params.OWNER,          "Incorrect fee recipient");
+        assertEq  (escrow.protocolFeeBps(), Params.BASE_FEE_BPS, "Incorrect fee");
+    }
+}
