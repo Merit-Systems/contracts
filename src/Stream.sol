05edd90b6b70b3a423dbd2689c0066ac4a7d6ce3
shafu <shafu0x@gmail.com>
2025-04-28 17:29:40 -0400

    Refactor token import and add create function

--- src/Stream.sol
+++ src/Stream.sol
@@ -1,18 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {ERC20}                        from "solmate/tokens/ERC20.sol";
+import {IERC20}                       from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ud60x18}                      from "@prb/math/src/UD60x18.sol";
 import {ISablierLockup}               from "@sablier/lockup/src/interfaces/ISablierLockup.sol";
 import {Broker, Lockup, LockupLinear} from "@sablier/lockup/src/types/DataTypes.sol";
 
 contract StreamCreator {
-    ERC20          public immutable token;
+    IERC20         public immutable token;
     ISablierLockup public immutable lockup;
 
     constructor(address _token, address _lockup) {
-        token  = ERC20(_token);
+        token  = IERC20(_token);
         lockup = ISablierLockup(_lockup);
     }
 
+    function create(
+        uint128 totalAmount,
+        address recipient,
+        uint40  duration
+    ) 
+        external 
+        returns (uint256 streamId)
+    {
+        token.transferFrom(msg.sender, address(this), totalAmount);
+        token.approve(address(lockup), totalAmount);
+
+        Lockup.CreateWithDurations memory params;
+
+        params.sender = msg.sender;
+        params.recipient = recipient;
+        params.totalAmount = totalAmount;
+        params.token = token;
+        params.cancelable = false;
+        params.transferable = false;
+
+        LockupLinear.UnlockAmounts memory unlockAmounts = LockupLinear.UnlockAmounts({ start: 0, cliff: 0 });
+        LockupLinear.Durations memory durations = LockupLinear.Durations({
+            cliff: 0, // Setting a cliff of 0
+            total: duration
+         });
+
+        streamId = lockup.createWithDurationsLL(params, unlockAmounts, durations);
+    }
 }
