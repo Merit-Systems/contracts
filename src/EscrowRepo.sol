18e1d02addd140b4d35ece2d1d3c7f9825b6ac41
shafu <shafu0x@gmail.com>
2025-06-06 13:52:11 +0200

    Update reclaiming of deposit in EscrowRepo contract

--- src/EscrowRepo.sol
+++ src/EscrowRepo.sol
@@ -387,15 +387,15 @@ contract EscrowRepo is Owned, IEscrowRepo {
     }
 
     function _reclaimDeposit(uint256 repoId, uint256 accountId, uint256 depositId) internal {
+        Deposit storage d = repos[repoId].deposits[accountId][depositId];
+
         require(depositId < repos[repoId].deposits[accountId].length, Errors.INVALID_CLAIM_ID);
+        require(d.status == Status.Deposited,                         Errors.ALREADY_CLAIMED);
+        require(block.timestamp > d.claimDeadline,                    Errors.STILL_CLAIMABLE);
 
-        Deposit storage _deposit = repos[repoId].deposits[accountId][depositId];
-        require(_deposit.status == Status.Deposited,      Errors.ALREADY_CLAIMED);
-        require(block.timestamp > _deposit.claimDeadline, Errors.STILL_CLAIMABLE);
-
-        _deposit.status = Status.Reclaimed;
-        repos[repoId].balance[accountId][address(_deposit.token)] += _deposit.amount;
-        emit Reclaimed(repoId, depositId, msg.sender, _deposit.amount);
+        d.status = Status.Reclaimed;
+        repos[repoId].balance[accountId][address(d.token)] += d.amount;
+        emit Reclaimed(repoId, depositId, msg.sender, d.amount);
     }
 
     /* -------------------------------------------------------------------------- */
