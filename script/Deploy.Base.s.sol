9aec812a5ada566d13f771d66b578d67776878d0
shafu <shafu0x@gmail.com>
2025-06-11 20:57:41 -0400

    Revert "Rename EscrowRepo to Escrow"

---
+++ script/Deploy.Base.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT	
+pragma solidity ^0.8.26;	
+
+import {Deploy} from "./Deploy.s.sol";	
+import {Params} from "../libraries/Params.sol";	
+import {Script} from "forge-std/Script.sol";	
+import {Escrow} from "../src/Escrow.sol";
+import {console} from "forge-std/console.sol";
+
+contract DeployBase is Deploy {	
+    function run() public returns (Escrow escrow) {	
+        address[] memory initialWhitelistedTokens = new address[](1);	
+        initialWhitelistedTokens[0] = Params.BASE_USDC;	
+
+        escrow = deploy(
+            Params.BASE_OWNER,
+            Params.BASE_SIGNER,
+            initialWhitelistedTokens,
+            Params.BASE_FEE_BPS,
+            Params.BATCH_DEPOSIT_LIMIT
+        );	
+
+        console.log("Escrow deployed at:", address(escrow));
+    }	
+}
---
+++ test/EscrowWithFee.t.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+import "forge-std/Test.sol";
+
+import {MockERC20}         from "solmate/test/utils/mocks/MockERC20.sol";
+import {ERC20}             from "solmate/tokens/ERC20.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+
+import {Escrow}        from "../src/Escrow.sol";
+import {DepositParams} from "../interface/IEscrow.sol";
+import {Params}        from "../libraries/Params.sol";
+import {Deploy}        from "../script/Deploy.s.sol";
+import {Errors}        from "../libraries/EscrowErrors.sol";
+
+contract Base_Test is Test {
+    using FixedPointMathLib for uint256;
+
+    Escrow escrow;
+
+    address alice;
+    address bob;
+
+    uint256 ownerPrivateKey = 0x4646464646464646464646464646464646464646464646464646464646464646;
+    address owner           = vm.addr(ownerPrivateKey);
+
+    MockERC20 wETH = new MockERC20("Wrapped Ether", "wETH", 18);
+
+    function setUp() public {
+        address[] memory initialWhitelistedTokens = new address[](1);
+        initialWhitelistedTokens[0] = address(wETH);
+        escrow = new Deploy().deploy(owner, owner, initialWhitelistedTokens, Params.BASE_FEE_BPS, Params.BATCH_DEPOSIT_LIMIT);
+
+        alice = makeAddr("alice");
+        bob   = makeAddr("bob");
+    }
+
+    function expectRevert(string memory message) public {
+        vm.expectRevert(bytes(message));
+    }
+
+    function test_deposit() public {
+        uint amount = 1000000000000000000;
+        uint depositId = deposit(amount, alice);
+        uint expectedBalance = amount - amount.mulDivUp(Params.BASE_FEE_BPS, 10_000);
+        assertEq(wETH.balanceOf(address(escrow)), expectedBalance);
+        assertEq(depositId, 0);
+    }
+
+     function test_fuzz_deposit(uint amount) public {
+        vm.assume(amount > 1e6 && amount < 1e32);
+        uint depositId = deposit(amount, alice);
+        assertEq(wETH.balanceOf(address(escrow)), amount - (amount.mulDivUp(Params.BASE_FEE_BPS, 10_000)));
+        assertEq(depositId, 0);
+    }
+
+    function test_deposit_1_USDC() public {
+        uint amount = 1e6;
+        uint depositId = deposit(amount, alice);
+        uint expectedBalance = amount - amount.mulDivUp(Params.BASE_FEE_BPS, 10_000);
+        assertEq(wETH.balanceOf(address(escrow)), expectedBalance);
+        assertEq(depositId, 0);
+    }
+
+    function test_claim() public {
+        uint amount = 1000000000000000000;
+        uint depositId = deposit(amount, alice);
+        (uint8 v, bytes32 r, bytes32 s) = generateSignature(alice, true);
+        escrow.claim(depositId, alice, true, 1 days, v, r, s);
+        uint expectedBalance = amount - amount.mulDivUp(Params.BASE_FEE_BPS, 10_000);
+        assertEq(wETH.balanceOf(alice), expectedBalance);
+    }
+
+    function test_reclaim() public {
+        uint amount = 1000000000000000000;
+        uint depositId = deposit(amount, alice);
+        vm.warp(block.timestamp + 2 days);
+        assertEq(wETH.balanceOf(bob), 0);
+        escrow.reclaim(depositId);
+        uint expectedBalance = amount - amount.mulDivUp(Params.BASE_FEE_BPS, 10_000);
+        assertEq(wETH.balanceOf(bob), expectedBalance);
+    }
+
+    function test_batchReclaim() public {
+        uint amount = 1000000000000000000;
+        uint[] memory depositIds = new uint[](1);
+        depositIds[0] = deposit(amount, alice);
+        vm.warp(block.timestamp + 2 days);
+        assertEq(wETH.balanceOf(bob), 0);
+        escrow.batchReclaim(depositIds);
+        uint expectedBalance = amount - amount.mulDivUp(Params.BASE_FEE_BPS, 10_000);
+        assertEq(wETH.balanceOf(bob), expectedBalance);
+    }
+
+    function deposit(uint amount, address recipient) public returns (uint depositId) {
+        wETH.mint(address(this), amount);
+        wETH.approve(address(escrow), amount);
+
+        DepositParams[] memory params = new DepositParams[](1);
+        params[0] = DepositParams({
+            token:       wETH,
+            sender:      bob,
+            recipient:   recipient,
+            amount:      amount,
+            claimPeriod: 1 days
+        });
+        return escrow.deposit(params[0]);
+    }
+
+    function generateSignature(address recipient, bool status) public view returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 structHash = keccak256(
+            abi.encode(
+                escrow.CLAIM_TYPEHASH(),
+                recipient,
+                status,
+                escrow.recipientNonces(recipient),
+                1 days
+            )
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", escrow.CLAIM_DOMAIN_SEPARATOR(), structHash)
+        );
+    
+        (v, r, s) = vm.sign(ownerPrivateKey, digest);
+    }
+}
